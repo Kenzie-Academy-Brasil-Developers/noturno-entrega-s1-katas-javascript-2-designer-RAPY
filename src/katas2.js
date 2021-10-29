436dfdc701ef4cafd3930e476f76f0de46ae8bbf
Ricardo Souza <86447805+designer-RAPY@users.noreply.github.com>
2021-10-28 21:15:54 -0300

    Update katas2.js

--- src/katas2.js
+++ src/katas2.js
@@ -1,28 +1,45 @@
 // comece a criar a sua função add na linha abaixo
-
+function add(num1,num2){
+    return num1 + num2
+}
 // descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
 
 // comece a criar a sua função multiply na linha abaixo
-
-
+function multiply(num1,num2){
+    let resultado=0
+    for(let contador=1;  contador <= num2 ; contador++){
+        resultado=add(num1,resultado);
+    }
+    return resultado
+}
 // descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
 
 // comece a criar a sua função power na linha abaixo
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
+function power(num1,num2){
+    let resultadoPower=1
+    for(let contador=1;  contador <= num2 ; contador++){
+        resultadoPower=multiply(num1,resultadoPower);
+    }
+    return resultadoPower
+}
+//descomente a linha seguinte para testar sua função
+//console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
 
 // comece a criar a sua função factorial na linha abaixo
-
-
+function factorial(argumento){
+    let resultadoFator=1
+        for(let contador=argumento; contador > 1 ; contador--){
+            resultadoFator=multiply(contador,resultadoFator);
+        }
+        return resultadoFator
+}
 // descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
+console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
 
 
 /**
